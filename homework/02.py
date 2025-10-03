c9d13b1cafeec44e27140b2fcd08b20895061949
gnaiX1005 <kmfc0711122@gmail.com>
2025-10-03 10:26:09 +0800

    02.py

--- homework/02.py
+++ homework/02.py
@@ -1,15 +1,17 @@
 import cmath
 
 def root2(a, b, c):
+    delta = b**2 - 4*a*c
+    root1 = (-b + cmath.sqrt(delta)) / (2*a)
+    root2 = (-b - cmath.sqrt(delta)) / (2*a)
 
-    d = b**2 - 4*a*c
+    f1 = a*root1**2 + b*root1 + c
+    f2 = a*root2**2 + b*root2 + c
 
-    sqrt_d = cmath.sqrt(d)
+    print(cmath.isclose(f1, 0, rel_tol=1e-9))
+    print(cmath.isclose(f2, 0, rel_tol=1e-9))
 
-    root1 = (-b + sqrt_d) / (2*a)
-    root2 = (-b - sqrt_d) / (2*a)
-    
     return root1, root2
 
-print(root2(1, -3, 2))   
-print(root2(1, 2, 5))    
+print(root2(1, -3, 2))
+print(root2(1, 2, 5))
